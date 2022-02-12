1178b3cece6ff4d79d5b049c5accf57558e2d54e
HoangZoan <d.hoang1906@gmail.com>
2022-02-12 15:25:19 +0700

    Section Cloud Functions: Update

---
+++ functions/index.js
@@ -0,0 +1,6 @@
+const FirebaseConfig = require("./FirebaseConfig");
+const functions = FirebaseConfig.functions;
+const auth = FirebaseConfig.auth;
+const firestore = FirebaseConfig.firestore;
+const storageBucket = FirebaseConfig.storageBucket;
+const admin = FirebaseConfig.admin;
